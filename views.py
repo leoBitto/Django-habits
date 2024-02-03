eb66c0b3e9e6d65715cd9cbcd280b0183fa0eaf5
leoBitto <leonardobitto@gmail.com>
2024-02-03 20:16:00 +0100

    8.11

--- views.py
+++ views.py
@@ -45,9 +45,9 @@ def test(request, start_date, end_date):
     # df = pd.DataFrame(correlation_values, index=habit_names, columns=habit_names)
 
     context={
-        'col1':type(df['habit__name'][0]),
-        'col2':type(df['date'][0]),
-        'col3':type(df['time'][0]),
+        'col1':type(df['habit__name']),
+        'col2':type(df['date']),
+        'col3':type(df['time']),
         
         'df': df.to_html(classes='table table-bordered', index=False),
         
